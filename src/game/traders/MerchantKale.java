9dd6c46effc9012c70186799c23167e0e9314d8c
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2023-05-01 16:27:40 +1000

    Changes to MerchantKale's PlayTurn method, MerchantKale does nothing

--- src/game/traders/MerchantKale.java
+++ src/game/traders/MerchantKale.java
@@ -1,11 +1,20 @@
 package game.traders;
 
+import edu.monash.fit2099.demo.mars.behaviours.SpitBehaviour;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.behaviour.Behaviour;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MerchantKale extends Trader {
+
+    private Map<Integer, Behaviour> behaviours = new HashMap<>();
+
     /**
      * Constructor.
      *
@@ -19,6 +28,19 @@ public class MerchantKale extends Trader {
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        return null;
+        // Loop through the actions
+        for (Behaviour behaviour : behaviours.values()) {
+
+            // Get action from the behaviours list
+            Action action = behaviour.getAction(this, map);
+
+            // If there is an action return it
+            if (action != null) {
+                return action;
+            }
+        }
+
+        // Otherwise, do nothing.
+        return new DoNothingAction();
     }
 }
